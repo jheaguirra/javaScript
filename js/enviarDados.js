16f6cbcaa7c6e7f46d9e5c7e0db2f9ec5acbefbe
jheaguirra <jhenifermartins.dev2024@gmail.com>
2025-03-25 14:04:32 -0300

    exercicio 13

--- js/enviarDados.js
+++
@@ -1,3 +0,0 @@
-function enviarDados(num1,num2){
-    resultado.textContent = parseFloat (num1 + num2)
-}
---
+++ js/mostrarDia.js
@@ -0,0 +1,7 @@
+function mostrarDia() {
+    let dia = document.getElementById("dia").value;
+    let dias = ["Domingo", "Segunda-feira", "Terça-feira", "Quarta-feira", "Quinta-feira", "Sexta-feira", "Sábado"];
+    document.getElementById("resultado").textContent = dias[dia - 1] || "Dia inválido!";
+}
+
+
